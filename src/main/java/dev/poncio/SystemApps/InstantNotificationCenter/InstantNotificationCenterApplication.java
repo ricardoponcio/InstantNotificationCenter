c9bd435554661e624a3aee5578ce4d62c4b2b945
Ricardo Vinicius Poncio <ricardo.poncio@velsis.com.br>
2021-09-28 13:54:40 -0300

    First Commit Backend and Frontend

---
+++ src/main/java/dev/poncio/SystemApps/InstantNotificationCenter/InstantNotificationCenterApplication.java
@@ -0,0 +1,13 @@
+package dev.poncio.SystemApps.InstantNotificationCenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstantNotificationCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstantNotificationCenterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/poncio/SystemApps/InstantNotificationCenter/InstantNotificationCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.poncio.SystemApps.InstantNotificationCenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstantNotificationCenterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
