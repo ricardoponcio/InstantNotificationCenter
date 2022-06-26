f7472684fb0ba51bd7b2b06bbaf3ef9fff56134b
Ricardo Poncio <ricardo.poncio@outlook.com.br>
2022-06-25 21:06:25 -0300

    [REF] Upgrade do Swagger para 2.10.5

--- src/main/java/dev/poncio/SystemApps/InstantNotificationCenter/configurations/SwaggerConfig.java
+++ src/main/java/dev/poncio/SystemApps/InstantNotificationCenter/configurations/SwaggerConfig.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -17,10 +16,9 @@ import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
-@EnableWebMvc
-@EnableSwagger2
+@EnableSwagger2WebMvc
 @Configuration
 public class SwaggerConfig {
 
